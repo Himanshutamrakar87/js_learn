1e2d0ac4ba5149f11e25069f1b512943de573be4
Himanshu Tamrakar <tamrakarh811@gmail.com>
2026-03-08 11:20:27 +0000

    about datat types

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,7 @@
+"use strict";   // the engine treat all js code as a new version 
+
+alert("hello")
+//alert(3+2)   this type only use in chrome not in nodejs system 
+
+console.log(3+2)
+ console.log("himanshu")
